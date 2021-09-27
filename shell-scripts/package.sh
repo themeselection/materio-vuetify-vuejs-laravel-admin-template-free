28522422d54f76d2501c1f8f0321a5228c34b246
Aloksoni <alok@pixinvent.com>
2021-09-27 12:38:38 +0530

    feat(scripts): shell script added

---
+++ shell-scripts/package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# use in linux/unix system
+# not properly work in git bash 
+echo 'You should run package.sh from parent folder. eg. ./scripts/package.sh'
+
+
+rsync -aP  ./ ../free-package/ --exclude=node_modules --exclude=vendor --exclude=yarn.lock --exclude=composer.lock --exclude=public/images --exclude=public/js --exclude=shell-scripts --exclude=.git    
+
+echo 'folders are compressing...'
+
+# add zip in linux/unix system before use it.
+# https://stackoverflow.com/questions/38782928/how-to-add-man-and-zip-to-git-bash-installation-on-windows
+
+zip -r ../free-package.zip ../free-package/
+
+echo 'Zip Completed!'
+
+rm -d -r ../free-package
+
+echo 'Task Completed!'
