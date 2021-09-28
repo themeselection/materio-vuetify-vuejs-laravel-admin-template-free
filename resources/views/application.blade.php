cab2ebf3d2c2bbc46eab5bb9eff31c29526b7819
Aloksoni <alok@pixinvent.com>
2021-09-28 17:00:26 +0530

    (refactor) title: Html title tag content changed

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -7,11 +7,12 @@
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>Materio - Vuetify Vuejs Admin Template</title>
+  <title>Materio-free</title>
 
   <!-- Favicon -->
   <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
   <link rel="preconnect" href="https://fonts.gstatic.com" />
+
   <!-- Font -->
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
 
