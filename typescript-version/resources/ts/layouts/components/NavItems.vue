49a52626e70bce7d3a1aaf79016bb93002cd28d1
alokVishu <alokpixinvent@gmail.com>
2024-01-05 17:26:45 +0530

    refactor: update dependencies and fix links in NavItems.vue and CardBasic.vue

--- typescript-version/resources/ts/layouts/components/NavItems.vue
+++ typescript-version/resources/ts/layouts/components/NavItems.vue
@@ -270,7 +270,7 @@ import VerticalNavLink from '@layouts/components/VerticalNavLink.vue'
     :item="{
       title: 'Documentation',
       icon: 'ri-article-line',
-      href: 'https://demos.themeselection.com/materio-vuetify-vuejs-admin-template/documentation/guide/laravel-integration/folder-structure.html',
+      href: 'https://demos.themeselection.com/materio-vuetify-vuejs-laravel-admin-template/documentation/',
       target: '_blank',
     }"
   />
